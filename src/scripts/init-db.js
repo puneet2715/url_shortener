e4278fc5e2be4daf2d86f7bc74710eea6f5e1083
Puneet Arora <puneet2715@gmail.com>
2025-02-22 01:14:56 +0530

    fix: script temrinates itself

--- src/scripts/init-db.js
+++ src/scripts/init-db.js
@@ -15,6 +15,9 @@ async function initializeDatabase() {
 
     // Close the pool
     await pool.end();
+    
+    // Exit successfully
+    process.exit(0);
   } catch (error) {
     console.error('Error initializing database:', error);
     process.exit(1);
