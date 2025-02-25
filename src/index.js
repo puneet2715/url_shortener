46ec3b878a2d0a8beac993453329b3bcc1e06a20
Puneet Arora <puneet2715@gmail.com>
2025-02-25 08:42:13 +0530

    updating the server for correct proxy interpretation

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const app = express();
 // Basic middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(helmet());
 
